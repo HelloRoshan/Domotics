73c2d421ebc0e36bd1bb4bc262167b100a2b12fc
Roshan Budhathoki <HelloRoshan@users.noreply.github.com>
2017-06-13 10:24:58 +0545

    First commit

---
+++ script1.py
@@ -0,0 +1,15 @@
+import RPIO as GPIO
+import time
+
+#refers to pins that is being referred are physical ins
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(7,GPIO.OUT)#sets pin 7 as an output
+
+for i in range(25):
+	GPIO.output(7,True)
+	time.sleep(1)#causes 1 seconds before  moving to other command
+	#energy saving with default time given after which it sleeps
+	GPIO.output(7,False)
+	time.sleep(1)
+
+GPIO.cleanup()
