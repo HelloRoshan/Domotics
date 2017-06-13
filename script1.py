7482e731f691525894af5c28a5010aad48427913
Roshan Budhathoki <HelloRoshan@users.noreply.github.com>
2017-06-13 10:30:05 +0545

    Second commit

--- script1.py
+++ script1.py
@@ -8,7 +8,6 @@ GPIO.setup(7,GPIO.OUT)#sets pin 7 as an output
 for i in range(25):
 	GPIO.output(7,True)
 	time.sleep(1)#causes 1 seconds before  moving to other command
-	#energy saving with default time given after which it sleeps
 	GPIO.output(7,False)
 	time.sleep(1)
 
