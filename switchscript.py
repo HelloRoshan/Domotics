a2455b7bda0dea4018e37d865dceb2fe29a7f00c
roshan101 <roshanuchiha02@gmail.com>
2017-08-21 08:44:01 +0000

    to update all the pins, be specific

--- switchscript.py
+++ switchscript.py
@@ -6,6 +6,7 @@ GPIO.setmode(GPIO.BCM)
 pinlist = [2, 3, 4, 14, 15, 17, 18, 22, 23, 24, 27]
 
 #loop through  pins
-for i in pinlist:
-    GPIO.setup(i, GPIO.OUT)
-    GPIO.output(i, not GPIO.input(i))
+click():
+    for i in pinlist:
+        GPIO.setup(i, GPIO.OUT)
+        GPIO.output(i, not GPIO.input(i))
